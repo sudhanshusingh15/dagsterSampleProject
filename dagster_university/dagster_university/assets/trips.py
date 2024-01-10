9ef72f24b0fef8e2047dd43081aea4b5d60f534b
sudhanshusingh <ss4827@drexel.edu>
2024-01-10 05:09:34 -0500

    created taxi_trips asset;  data pipeline btw taxi_trips_file, taxi_trips

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,3 +1,5 @@
+import duckdb
+import os
 import requests
 from . import constants
 from dagster import asset
@@ -29,3 +31,30 @@ def taxi_zones_file():
         output_file.write(raw_taxi_zones.content)
 
 
+
+@asset(
+	deps=["taxi_trips_file"]
+)
+def taxi_trips():
+		"""
+        The raw taxi trips dataset, loaded into a DuckDB database
+    """
+		sql_query = """
+				create or replace table trips as (
+						select
+								VendorID as vendor_id,
+								PULocationID as pickup_zone_id,
+								DOLocationID as dropoff_zone_id,
+								RatecodeID as rate_code_id,
+								payment_type as payment_type,
+								tpep_dropoff_datetime as dropoff_datetime,
+								tpep_pickup_datetime as pickup_datetime,
+								trip_distance as trip_distance,
+								passenger_count as passenger_count,
+								total_amount as total_amount
+						from 'data/raw/taxi_trips_2023-03.parquet'
+				);
+		"""
+
+		conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+		conn.execute(sql_query)
