e4897d7b4b8f5be557533d663169cb8f9b4f7238
sudhanshusingh <ss4827@drexel.edu>
2024-01-09 21:31:30 -0500

    created assets: taxi_trips_file, taxi_zones_file

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,2 +1,31 @@
 import requests
 from . import constants
+from dagster import asset
+
+@asset
+def taxi_trips_file():
+    """The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal."""
+    month_to_fetch = "2023-03"
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(
+        constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
+    ) as output_file:
+        output_file.write(raw_trips.content)
+
+
+@asset
+def taxi_zones_file():
+    """ The raw parquet files for the taxi zones dataset. Sourced from the NYC Open Data portal."""
+    raw_taxi_zones = requests.get(
+        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open(
+        constants.TAXI_ZONES_FILE_PATH, "wb"
+    ) as output_file:
+        output_file.write(raw_taxi_zones.content)
+
+
